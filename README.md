f9af598efdb3120b546bbd29f85e6ba95e306e66
dsnatochy <dennis@poynt.co>
2016-01-11 16:41:10 -0800

    Added server timeout 60 seconds and updated README

--- README.md
+++ README.md
@@ -1,26 +1,15 @@
-POSConnector is a sample app which facilitates communication between a POS app running on a tablet and a Poynt Smart Terminal over a TLS connection.
-
-The app has a sample self-signed cert used by the SSLServerSocket in assets/ServerKeystore.bks.
-
-To generate your own keystore you will first need to download Bouncycastle jar and then generate the keystore using the following command:
-
-keytool -genkey -keystore ServerKeystore.bks -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath <path to jar>/bcprov-jdk15on-146.jar
+POSConnector is a sample app which facilitates communication between a POS app running on a tablet and a Poynt Smart Terminal over a TLS connection with client side certificate authentication.
 
 To run a quick test:
 
 - Install the app on a Poynt device
 - Find IP address of your Poynt terminal using the following command: adb shell netcfg
-- Run the following OpenSSL command from terminal (assuming terminal IP is 192.168.1.2): openssl s_client -connect 192.168.1.2:60000
-- Once the connection is established type a simple JSON request: {"action":"sale","purchaseAmount":1000,"currency":"USD","referenceId":"yourOrderId"} and press ENTER
-- The Payment Fragment should come up on the terminal and after the payment is completed your terminal window should get a response in the form of a Payment object.
+- Download python client from https://github.com/dsnatochy/posconnectorpythonclient
+- Run the client `python posconnectorclient.py`
+- The Payment Fragment should come up on the terminal and after the payment is completed your terminal window should get a response in the form of a Payment json object.
 
 
-- generate private key for the POS client: openssl genrsa -des3 -out client_key.pem 2048
-- generate certificate for the POS client: openssl req -new -x509 -key client_key.pem -out clientcert.pem -days 365
-- add client cert as a trusted cert to the trustore for POS Connector app: keytool -importcert -trustcacerts -keystore servertruststore.bks -storetype bks -storepass 123456 -file clientcert.pem -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath <path to Bouncycastle lib>/bcprov-jdk15on-146.jar
-- Connecting to Poynt terminal using the client cert: openssl s_client -connect <deviceip>:60000 -cert clientcert.pem -key client_key.pem 
-
-
+JSON request sent by the client:
 ```
 {
 	"referenceId":"myRefId_12342",
@@ -76,3 +65,21 @@ To run a quick test:
 	}
 }
 ```
+
+The POSConnector has a sample self-signed cert used by the SSLServerSocket in keystore in assets/ServerKeystore.bks.
+
+If you need to generate your own keystore you will first need to download Bouncycastle jar (https://www.bouncycastle.org/download/bcprov-jdk15on-146.jar) and then generate the keystore using the following command:
+
+keytool -genkey -keystore ServerKeystore.bks -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath <path to jar>/bcprov-jdk15on-146.jar
+
+Your client will need to have the server certificate in its truststore. To extract server certificate from the server keystore you can run the following command:
+
+keytool -exportcert -rfc -keystore <path>/ServerKeystore.bks -file <servercert>.pem -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath <path>/bcprov-jdk15on-146.jar
+
+To generate generate certificate and private key for the client:
+
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
+
+Client certificate need to be trusted by the server. To import the client certificate into server truststore:
+
+keytool -import -trustcacerts -keystore <path>/assets/servertruststore.bks -storepass <pass> -noprompt -alias mycert -file <path>/cert.crt
--- app/src/main/java/co/poynt/samples/posconnector/MyService.java
+++ app/src/main/java/co/poynt/samples/posconnector/MyService.java
@@ -16,17 +16,22 @@ import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.security.KeyStore;
+import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.Gson;
+import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
@@ -54,17 +59,17 @@ public class MyService extends Service {
     private SSLServerSocket serverSocket;
     // to ensure only one request is in flight at a time
     ExecutorService fixedPool = Executors.newFixedThreadPool(1);
-
     private Thread serverThread;
     // payment status and details
     private Payment paymentResult;
-    // broadcast received notified when payment is completed
+    // broadcast receiver notified when payment is completed
     private PaymentResultReceiver paymentResultReceiver;
-
     private Context context = this;
-    private Object LOCK = this;
-
+    private HashMap<String, BlockingQueue<Payment>> requestMap;
+    //private BlockingQueue<Payment> LOCK;
     private IPoyntOrderService mOrderService;
+
+    private final int REQUEST_TIMEOUT=60;
     /**
      * Class for interacting with the OrderService
      */
@@ -75,6 +80,7 @@ public class MyService extends Service {
             // this gets an instance of the IRemoteInterface, which we can use to call on the service
             mOrderService = IPoyntOrderService.Stub.asInterface(service);
         }
+
         // Called when the connection with the service disconnects unexpectedly
         public void onServiceDisconnected(ComponentName className) {
             Log.d(TAG, "PoyntOrderService has unexpectedly disconnected");
@@ -87,7 +93,8 @@ public class MyService extends Service {
             Log.d("orderListener", "poyntError: " + (poyntError == null ? "" : poyntError.toString()));
         }
     };
-    private class SaveOrderTask extends AsyncTask<Order, Void, Void>{
+
+    private class SaveOrderTask extends AsyncTask<Order, Void, Void> {
         protected Void doInBackground(Order... params) {
             Order order = params[0];
             String requestId = UUID.randomUUID().toString();
@@ -102,8 +109,9 @@ public class MyService extends Service {
         }
     }
 
-    public void onCreate(){
+    public void onCreate() {
         Log.d(TAG, "MyService created");
+        requestMap = new HashMap<>();
 
         bindService(new Intent(IPoyntOrderService.class.getName()), mOrderServiceConnection, Context.BIND_AUTO_CREATE);
 
@@ -117,17 +125,18 @@ public class MyService extends Service {
         paymentResultReceiver = new PaymentResultReceiver();
         registerReceiver(paymentResultReceiver, intentFilter);
     }
-    public void onDestroy(){
+
+    public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(paymentResultReceiver);
         unbindService(mOrderServiceConnection);
     }
 
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
-    public int onStartCommand (Intent intent, int flags, int startId){
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
         startForeground(1, getNotification());
         return START_STICKY;
     }
@@ -140,14 +149,22 @@ public class MyService extends Service {
         return notification;
     }
 
-    class PaymentResultReceiver extends BroadcastReceiver{
+    class PaymentResultReceiver extends BroadcastReceiver {
         public void onReceive(Context context, Intent intent) {
             paymentResult = intent.getParcelableExtra(Intents.INTENT_EXTRAS_PAYMENT);
-            synchronized (LOCK) {
-                LOCK.notify();
+            try {
+                BlockingQueue<Payment> LOCK = requestMap.get(paymentResult.getReferenceId());
+                // LOCK will be null if request timed out and server closed connection
+                if (LOCK != null){
+                    LOCK.put(paymentResult);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
+
     class ServerThread implements Runnable {
         public void run() {
             SSLSocket socket = null;
@@ -176,16 +193,17 @@ public class MyService extends Service {
                 SSLContext context = SSLContext.getInstance("TLS");
                 //context.init(kmf.getKeyManagers(), null, null);
                 context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-                serverSocket = (SSLServerSocket)context.getServerSocketFactory().createServerSocket(SERVERPORT);
+                serverSocket = (SSLServerSocket) context.getServerSocketFactory().createServerSocket(SERVERPORT);
                 // to require client side cert
                 serverSocket.setNeedClientAuth(true);
+                //serverSocket.setWantClientAuth(true);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
             while (!Thread.currentThread().isInterrupted()) {
                 try {
-                    socket = (SSLSocket)serverSocket.accept();
+                    socket = (SSLSocket) serverSocket.accept();
                     CommunicationThread commThread = new CommunicationThread(socket);
                     fixedPool.execute(commThread);
                 } catch (IOException e) {
@@ -195,67 +213,93 @@ public class MyService extends Service {
         }
     }
 
-    class CommunicationThread implements Runnable{
+    class CommunicationThread implements Runnable {
         private SSLSocket socket;
         BufferedReader input;
-        public CommunicationThread(SSLSocket s){
-            socket=s;
-            try{
+
+        public CommunicationThread(SSLSocket s) {
+            socket = s;
+            try {
                 input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
         public void run() {
-            try{
+            String referenceId = null;
+            try {
+
                 String inputString = input.readLine();
-
                 Gson gson = new Gson();
-
-                Type typeRequestResponse = new TypeToken<Payment>(){}.getType();
+                Type typeRequestResponse = new TypeToken<Payment>() {}.getType();
                 Payment posPaymentRequest = gson.fromJson(inputString, typeRequestResponse);
 
-                Intent dummyActivityIntent = new Intent (context, DummyTransparentActivity.class);
+                referenceId = posPaymentRequest.getReferenceId();
+                // if reference id is not passed, generate one
+                if (referenceId == null || "".equals(referenceId)){
+                    referenceId = UUID.randomUUID().toString();
+                    posPaymentRequest.setReferenceId(referenceId);
+                }
+
+                BlockingQueue<Payment> LOCK = new ArrayBlockingQueue<Payment>(1);
+                requestMap.put(posPaymentRequest.getReferenceId(), LOCK);
+
+                Intent dummyActivityIntent = new Intent(context, DummyTransparentActivity.class);
                 // Intent.FLAG_ACTIVITY_NEW_TASK is needed because it's a new activity launched from service
                 dummyActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 dummyActivityIntent.putExtra("request", posPaymentRequest);
                 startActivity(dummyActivityIntent);
-                synchronized (LOCK) {
-                    while (paymentResult == null) {
-                        try {
-                            // wait until the payment result receiver gets notified that payment completed
-                            LOCK.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
 
-                // if payment is canceled we need to set the original referenceId
-                if (paymentResult.getStatus().equals(PaymentStatus.CANCELED)){
+                try {
+                    // wait up to 60 seconds to get paymentResult from the payment fragment
+                    paymentResult = LOCK.poll(REQUEST_TIMEOUT, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // clear the queue
+                if (!LOCK.isEmpty()) LOCK.take();
+                // this would happen if payment activity did not finish in 60 seconds
+                if (paymentResult == null){
+                    paymentResult = new Payment();
+                    paymentResult.setStatus(PaymentStatus.FAILED);
+                    paymentResult.setReferenceId(posPaymentRequest.getReferenceId());
+                }
+                // if payment is canceled or failed we need to set the original referenceId
+                if (paymentResult.getStatus().equals(PaymentStatus.CANCELED) ||
+                        paymentResult.getStatus().equals(PaymentStatus.FAILED)) {
                     // when payment is canceled the response from Payment Fragment does not have reference id
                     // therefore setting it using reference id from the request
                     paymentResult.setReferenceId(posPaymentRequest.getReferenceId());
-                }else{
+                } else {
                     // this means we had a successful payment and can save order
                     if (paymentResult.getOrder() != null) {
                         new SaveOrderTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, paymentResult.getOrder());
                     }
                 }
                 String result = gson.toJson(paymentResult, typeRequestResponse);
-                socket.getOutputStream().write(result.getBytes());
-            } catch (Exception e){
+                OutputStream os = socket.getOutputStream();
+                os.write(result.getBytes());
+                os.flush();
+                //TODO should separate this into it's own try/catch to provide better messaging
+                // 2nd write and flush will trigger exception if the client disconnected
+                os.write("\n".getBytes());
+                os.flush();
+            } catch (Exception e) {
                 Handler handler = new Handler(Looper.getMainLooper());
                 final String errorMessage = e.getMessage();
-                handler.post(new Runnable(){
+                handler.post(new Runnable() {
                     public void run() {
-                        Toast.makeText(MyService.this, "Exception: " + errorMessage + ". Check the logs", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MyService.this, "Exception: " + errorMessage + ". Check the logs", Toast.LENGTH_LONG).show();
                     }
                 });
                 e.printStackTrace();
-            } finally{
+            } finally {
                 // set paymentResult back to null
                 paymentResult = null;
+
+                requestMap.remove(referenceId);
+
                 try {
                     input.close();
                     socket.close();
